e5c8d011d889b47f386accae4aa6113d35f65028
akchonya <akchonyahokagivna@gmail.com>
2023-11-17 02:11:38 +0200

    initial main.py commit

---
+++ main.py
@@ -0,0 +1,94 @@
+"""
+This example shows how to use webhook on behind of any reverse proxy (nginx, traefik, ingress etc.)
+"""
+import logging
+import sys
+from os import getenv
+from dotenv import load_dotenv
+
+from aiohttp import web
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+
+load_dotenv()
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+WEBHOOK_SECRET = getenv("WEBHOOK_SECRET")
+BASE_WEBHOOK_URL = getenv("BASE_WEBHOOK_URL")
+WEB_SERVER_HOST = getenv("WEB_SERVER_HOST")
+
+
+# Port for incoming request from reverse proxy. Should be any available port
+WEB_SERVER_PORT = getenv("WEB_SERVER_PORT")
+
+# Path to webhook route, on which Telegram will send requests
+WEBHOOK_PATH = getenv("WEBHOOK_PATH")
+
+# All handlers should be attached to the Router (or Dispatcher)
+router = Router()
+
+
+@router.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer(
+        "👋🏻 привіт всім! це ботік для каналу @ua_hackathons\n\n"
+        "наразі він взагалі нічого не вміє робити так шо всєм *стікєр майнкрафт пока*"
+    )
+
+
+# @router.message()
+# async def echo_handler(message: types.Message) -> None:
+#     await message.answer("🔝 Main Menu")
+
+
+async def on_startup(bot: Bot) -> None:
+    # If you have a self-signed SSL certificate, then you will need to send a public
+    # certificate to Telegram
+    await bot.set_webhook(
+        f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET
+    )
+
+
+def main() -> None:
+    # Dispatcher is a root router
+    dp = Dispatcher()
+    # ... and all other routers should be attached to Dispatcher
+    dp.include_router(router)
+
+    # Register startup hook to initialize webhook
+    dp.startup.register(on_startup)
+
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
+
+    # Create aiohttp.web.Application instance
+    app = web.Application()
+
+    # Create an instance of request handler,
+    # aiogram has few implementations for different cases of usage
+    # In this example we use SimpleRequestHandler which is designed to handle simple cases
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=WEBHOOK_SECRET,
+    )
+    # Register webhook handler on application
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+
+    # Mount dispatcher startup and shutdown hooks to aiohttp application
+    setup_application(app, dp, bot=bot)
+
+    # And finally start webserver
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    main()
